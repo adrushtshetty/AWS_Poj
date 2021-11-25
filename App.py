f941658409ebbf610d97e08b1267457ce8f6a5a1
Adrusht Shetty <adrushtshetty@gmail.com>
2021-11-25 13:53:52 +0530

    Model.pkl

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template, url_for
 import pickle
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('Model.pklodel.pkl', 'rb'))
 
 @app.route('/')
 def home_page():
